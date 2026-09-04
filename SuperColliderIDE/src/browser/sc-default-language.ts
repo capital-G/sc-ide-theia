0235148da2d53e1ad6d0ad1773ec7df3f9301ecc
Dennis Scheiba <git@dennis-scheiba.com>
2026-09-04 20:07:54 +0200

    open scratch document when no document is opened

--- SuperColliderIDE/src/browser/sc-default-language.ts
+++ SuperColliderIDE/src/browser/sc-default-language.ts
@@ -1,4 +1,4 @@
-import { UNTITLED_SCHEME } from "@theia/core";
+import { UNTITLED_SCHEME, UntitledResourceResolver } from "@theia/core";
 import { FrontendApplicationContribution } from "@theia/core/lib/browser";
 import { inject, injectable } from "@theia/core/shared/inversify";
 import { EditorManager } from "@theia/editor/lib/browser";
@@ -10,6 +10,7 @@ import { SC_LANGUAGE_ID } from "./sc-language-contribution";
 @injectable()
 export class ScDefaultLanguage implements FrontendApplicationContribution {
     @inject(EditorManager) protected readonly editorManager!: EditorManager;
+    @inject(UntitledResourceResolver) protected readonly untitled!: UntitledResourceResolver;
     
     onStart(): void {
         this.editorManager.onCreated(widget => {
@@ -18,5 +19,13 @@ export class ScDefaultLanguage implements FrontendApplicationContribution {
                 editor.setLanguage(SC_LANGUAGE_ID);
             }
         })
+    }
+    // after layout has been restored, check if there are any open documents
+    // if not, lets open a scratch pad for the user
+    async onDidInitializeLayout(): Promise<void> {
+        if (this.editorManager.all.length > 0) { return; }
+        const uri = this.untitled.createUntitledURI(".scd");
+        await this.untitled.resolve(uri);
+        await this.editorManager.open(uri);
     }
 }
